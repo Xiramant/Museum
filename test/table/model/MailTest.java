65137de10d074011aadd6a4ab5bcc208511d2fa3
xiramant <artnic7@yandex.ru>
2018-10-21 22:59:58 +0600

    created test for Mail class

---
+++ test/table/model/MailTest.java
@@ -0,0 +1,17 @@
+package table.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static table.model.Mail.*;
+
+public class MailTest {
+
+    @Test
+    public void testSetMailsList() {
+
+        setMailsScene();
+
+        assertTrue(true);
+    }
+}
