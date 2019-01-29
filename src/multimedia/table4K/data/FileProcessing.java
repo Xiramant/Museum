41db77b94c841d91f09ad91baad6eb86f20c50df
Xiramant <artnic7@yandex.ru>
2019-01-29 14:50:23 +0600

    Deleted "getFilterFiles" method in "FileProcessing" class and use instead "listFiles(FilenameFilter filter)" method.

--- src/multimedia/table4K/data/FileProcessing.java
+++ src/multimedia/table4K/data/FileProcessing.java
@@ -4,6 +4,7 @@ import general.FileFormat;
 import javafx.scene.image.Image;
 
 import java.io.File;
+import java.net.MalformedURLException;
 import java.util.ArrayList;
 
 
@@ -12,10 +13,17 @@ public class FileProcessing {
 
     //получение изображений с названием определенного формата из указанной папки
     public static ArrayList<Image> getImages(final File dirArg) {
-        ArrayList<File> imageFiles = getFiles(dirArg,  FileFormat.IMAGE);
+        File[] imageFiles = getFiles(dirArg,  FileFormat.IMAGE);
         ArrayList<Image> images = new ArrayList<>();
+        
         for (File imageFile: imageFiles) {
-            images.add(new Image("file:///" + imageFile));
+            try {
+                images.add(new Image(imageFile.toURI().toURL().toString()));
+            }
+            catch (MalformedURLException e) {
+                System.out.println("Ошибка преобразования файла в путь");
+                e.printStackTrace();
+            }
         }
 
         return images;
@@ -23,29 +31,8 @@ public class FileProcessing {
 
     //Получение списка файлов из указанной директории (первый аргумент)
     // определенного типа (второй аргумент)
-    private static ArrayList<File> getFiles(final File dirArg, final FileFormat formatArg) {
-        File[] allDirFiles = dirArg.listFiles();
-        if (allDirFiles.length == 0) {
-            return null;
-        }
-
-        return getFilterFiles(allDirFiles, formatArg.getKeyWord() + "_");
-    }
-
-    //Фильтрация файлов по переданному формату
-    private static ArrayList<File> getFilterFiles (final File[] inputFilesListArg, final String filterArg) {
-        if (inputFilesListArg.length == 0) {
-            return null;
-        }
-
-        ArrayList<File> outputFilesList = new ArrayList<>();
-        for (File inputFile: inputFilesListArg) {
-            if (inputFile.toString().contains(filterArg)) {
-                outputFilesList.add(inputFile);
-            }
-        }
-
-        return outputFilesList;
+    private static File[] getFiles(final File dirArg, final FileFormat formatArg) {
+        return dirArg.listFiles((dirPath, fileName) -> fileName.contains(formatArg.getKeyWord() + "_"));
     }
 
 }
