f6a3493e8541bff6898c3c395438bd515b5b95aa
Xiramant <artnic7@yandex.ru>
2019-01-31 10:20:14 +0600

    Change argument in "createImage" method in "FileProcessing" class.

--- src/multimedia/table4K/data/IconData.java
+++ src/multimedia/table4K/data/IconData.java
@@ -2,6 +2,7 @@ package table4K.data;
 
 import javafx.scene.image.Image;
 
+import static table4K.Main4K.RESOURCES_PATH;
 import static table4K.data.FileProcessing.createImage;
 
 
@@ -9,34 +10,34 @@ import static table4K.data.FileProcessing.createImage;
 public class IconData {
 
     public static Image getBookIconImage() {
-        return createImage("icon/book_icon.png");
+        return createImage(RESOURCES_PATH + "icon/book_icon.png");
     }
 
     public static Image getVideoIcon1Image() {
-        return createImage("icon/video_icon_1.png");
+        return createImage(RESOURCES_PATH + "icon/video_icon_1.png");
     }
     public static Image getVideoIcon2Image() {
-        return createImage("icon/video_icon_2.png");
+        return createImage(RESOURCES_PATH + "icon/video_icon_2.png");
     }
 
     public static Image getMailIconImage() {
-        return createImage("icon/mail_icon.png");
+        return createImage(RESOURCES_PATH + "icon/mail_icon.png");
     }
 
     public static Image getMapIconImage() {
-        return createImage("icon/map_icon.png");
+        return createImage(RESOURCES_PATH + "icon/map_icon.png");
     }
 
     public static Image getMedalIconImage() {
-        return createImage("icon/medal_icon.png");
+        return createImage(RESOURCES_PATH + "icon/medal_icon.png");
     }
 
     public static Image getPortfolioIconImage() {
-        return createImage("icon/portfolio_icon.png");
+        return createImage(RESOURCES_PATH + "icon/portfolio_icon.png");
     }
 
     public static Image getQuizIconImage() {
-        return createImage("icon/quiz_icon.png");
+        return createImage(RESOURCES_PATH + "icon/quiz_icon.png");
     }
 
 }
--- src/multimedia/table4K/data/RootPaneBackgroundData.java
+++ src/multimedia/table4K/data/RootPaneBackgroundData.java
@@ -2,15 +2,18 @@ package table4K.data;
 
 import javafx.scene.image.Image;
 
+import static table4K.Main4K.RESOURCES_PATH;
+import static table4K.data.FileProcessing.createImage;
+
 
 public class RootPaneBackgroundData {
 
     public static Image getSectionSelectionBackground() {
-        return FileProcessing.createImage("table_with_lamp.jpg");
+        return createImage(RESOURCES_PATH + "table_with_lamp.jpg");
     }
 
     public static Image getBookBackground() {
-        return FileProcessing.createImage("table_4K_book.jpg");
+        return createImage(RESOURCES_PATH + "table_4K_book.jpg");
     }
 
 }
