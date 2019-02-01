9108b74aa7cb293ffab62ee747ac8ed71eedb63b
Xiramant <artnic7@yandex.ru>
2019-02-01 08:22:48 +0600

    Refactoring "VideoData" class.

--- src/multimedia/table4K/data/video/VideoData.java
+++ src/multimedia/table4K/data/video/VideoData.java
@@ -18,31 +18,41 @@ public class VideoData {
     //путь к директории с фильмами
     private static final String VIDEO_PATH = RESOURCES_PATH + "video/videos/";
 
-    //часть пути к изображению коробки для видео
+    //путь к изображению коробки для видео
     private static final String VIDEO_BOX_PATH = RESOURCES_PATH + "video/video_box.png";
+
+    //путь к изображению фона для сцены Video List (Перечень видео)
+    private static final String VIDEO_LIST_BACKGROUND = RESOURCES_PATH + "table_4K_film_initial.jpg";
 
 
 
     public static ArrayList<VideoContent> getVideoContentList() {
-        ArrayList<VideoContent> out = new ArrayList<>();
+        ArrayList<VideoContent> outList = new ArrayList<>();
+        ArrayList<File> videoFileList = getVideoFileList();
 
-        ArrayList<File> videoFiles = getFilesCertainType(new File(VIDEO_PATH), FileType.VIDEO);
-
-        for (File videoFile: videoFiles) {
-            Media videoMedia = new Media(videoFile.toURI().toString());
-            String videoName = getName(videoFile);
-            out.add(new VideoContent(videoMedia, videoName));
+        for (File videoFile: videoFileList) {
+            outList.add(new VideoContent(getMedia(videoFile), getName(videoFile)));
         }
 
-        return out;
+        return outList;
     }
+
+    private static ArrayList<File> getVideoFileList() {
+        return getFilesCertainType(new File(VIDEO_PATH), FileType.VIDEO);
+    }
+
+    private static Media getMedia(final File videoFileArg) {
+        return new Media(videoFileArg.toURI().toString());
+    }
+
+
 
     public static Image getVideoBoxImage() {
         return createImage(VIDEO_BOX_PATH);
     }
 
     public static Image getVideoListBackground() {
-        return createImage(RESOURCES_PATH + "table_4K_film_initial.jpg");
+        return createImage(VIDEO_LIST_BACKGROUND);
     }
 
 }
